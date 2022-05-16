3319afde972c1812200551f23016f146459f0624
Atalay Ozkan <matalayath@gmail.com>
2022-05-16 17:44:33 +0300

    Initial commit

---
+++ src/com/bilesim/net/Main.java
@@ -0,0 +1,12 @@
+package com.bilesim.net;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello Taluhan");
+        System.out.println("Welcome to DevOps");
+
+
+    }
+}
